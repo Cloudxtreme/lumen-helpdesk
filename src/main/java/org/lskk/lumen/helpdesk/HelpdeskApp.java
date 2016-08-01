1a34686df433ae4fd9fa1fbead9e8dc7b56a3092
gentry-swanri <gentryswanri@yahoo.com>
2016-08-01 21:15:58 +0700

    -) read csv file through java -) connect postgre through java -) connect elasticsearch through java

--- src/main/java/org/lskk/lumen/helpdesk/HelpdeskApp.java
+++ src/main/java/org/lskk/lumen/helpdesk/HelpdeskApp.java
@@ -6,14 +6,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
+import java.util.LinkedList;
+
 /**
  * Aplikasi utama untuk menjalankan helpdesk.
  */
-@SpringBootApplication(exclude = {GroovyTemplateAutoConfiguration.class})
+@SpringBootApplication(exclude = {GroovyTemplateAutoConfiguration.class, LiquibaseAutoConfiguration.class})
 @Profile({"helpdeskApp"})
 //@Import(LumenCoreConfig.class)
 class HelpdeskApp implements CommandLineRunner {
@@ -21,9 +24,27 @@ class HelpdeskApp implements CommandLineRunner {
     private static final Logger log = LoggerFactory.getLogger(HelpdeskApp.class);
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(HelpdeskApp.class)
-                .profiles("helpdeskApp"/*, "rabbitmq", "drools"*/)
-                .run(args);
+//        new SpringApplicationBuilder(HelpdeskApp.class)
+//                .profiles("helpdeskApp"/*, "rabbitmq", "drools"*/)
+//                .run(args);
+
+//        CSVParser parser = new CSVParser();
+//        parser.parseCSV("twitter.csv");
+//        LinkedList data = parser.getData();
+
+        PostgreConnector pc = new PostgreConnector();
+        pc.startConnection();
+        LinkedList postgreData = pc.selectOperation("lumen.twitterstatus");
+        pc.endConnection();
+
+        ElasticSearchConnector esConnector = new ElasticSearchConnector();
+        esConnector.startConnection();
+        for(int i=0; i<postgreData.size(); i++) {
+            String text = (String)postgreData.get(i);
+            double maxScore = esConnector.search("logstash-2016.07.27", "_all", text);
+            System.out.println(text +" max score = "+ maxScore);
+        }
+        esConnector.endConnection();
     }
 
     @Override
