87523c309fc0bf13f65f25a941a942d5e25cdddb
Hendy Irawan <ceefour666@gmail.com>
2016-08-20 12:14:36 +0700

    logging

--- helpdesk.sh
+++ helpdesk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname $0)"
-java -Xms1g -Xmx1g -Djava.awt.headless=true -cp $SCRIPT_DIR'/target/classes':$SCRIPT_DIR'/target/dependency/*' org.lskk.lumen.helpdesk.HelpdeskApp "$@"
+java -Xmx3g -Djava.awt.headless=true -cp $SCRIPT_DIR'/target/classes':$SCRIPT_DIR'/target/dependency/*' org.lskk.lumen.helpdesk.HelpdeskApp "$@"
